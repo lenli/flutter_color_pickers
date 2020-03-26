68d2803254029a920643f65f8ff914f4342d1fc4
Username <leonard.li@gmail.com>
2020-03-25 20:36:35 -0400

    Convert to package

--- flutter_color_pickers.dart
+++
@@ -1,4 +0,0 @@
-library flutter_color_pickers;
-
-export 'src/bar_color_picker.dart' show BarColorPicker;
-export 'src/circular_color_picker.dart' show CircularColorPicker;
---
+++ lib/flutter_color_pickers.dart
@@ -0,0 +1,4 @@
+library fluttercolorpickers;
+
+export 'src/bar_color_picker.dart' show BarColorPicker;
+export 'src/circular_color_picker.dart' show CircularColorPicker;
